a64a482491f0f7955f4fda9c5bc4f34e2be3be30
Admin cloudmytribe <admin@cloudmytribe.com>
2024-06-11 09:31:37 +0000

    Demonstrate variable naming convention

---
+++ Data_types/general.py
@@ -0,0 +1,21 @@
+# Variables -> rules 
+
+# avoid using python keywords
+
+greeting = "hello world"
+
+# underscores, letters and numbers | avoid spaces
+
+number_1 = 10
+
+student_1 = "Zadock"
+s_1 = "Zadock"
+
+# lowercase 
+apples = 4
+
+APPLES = 4
+
+# class Car()
+
+
